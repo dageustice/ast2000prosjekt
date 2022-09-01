7a155c8d974fd026f32835e2c34c46073ab72e2b
JosephAbeb <112571388+JosephAbeb@users.noreply.github.com>
2022-09-01 21:28:42 +0200

    Update Oppgave 2.py

--- Oppgave 2.py
+++ Oppgave 2.py
@@ -1,31 +1,76 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import random
-def Q(a, b, N, T):
+def Pvx(a, b, N, T):
     Vx = np.linspace(a, b, N)  # Hastighet for N partikler i x retning
     m = 2*1.6e-27
     K = 1.38*1e-23
     o = (K*T)
-    sigma = 3520.68
-    Dens = np.zeros(len(Vx))
+    sigma = np.sqrt(o/m)
+    Densx = np.zeros(len(Vx))
     for i in range(len(Vx)):
         # sannsynlighets tettheten for alle Vx verdier av N partikler
-        # Dens[i] = ((m/(np.square(2*np.pi*K*T))))*np.exp((-0.5*m*Vx[i]**2)/(K*T))
-        Dens[i] = 1/(np.sqrt(2*np.pi)*sigma)*np.exp(-Vx[i]**2/(2*sigma**2))
+        Densx[i] = 1/(np.sqrt(2*np.pi)*sigma)*np.exp(-Vx[i]**2/(2*sigma**2))
         # Dens[i] = (1/(np.square(2*np.pi)*(sigma)))*(np.exp(-Vx[i]**2/(2*(sigma)**2)))
-    return Vx, Dens
+    return Vx, Densx
 
-
-sigma = 3520.68
 # Vx, Dens = Q(-2.5e4, 2.5e4)
-Vx, Dens = Q(5e3, 30e3, 10**5, 3000)
+N=10**5
+Vx, Densx = Pvx(-2.5e4, 2.5e4, N, 3000)
 Integral = np.zeros(len(Vx+1))
 dx = Vx[1]-Vx[0]
-integrate = np.trapz(Dens, Vx, dx)
+integrate = np.trapz(Densx, Vx, dx)
 for i in range(len(Vx)-1):
-    Integral[i] = dx*(1/2)*(Dens[i]+Dens[i+1])
-print(sum(Integral))
-
-plt.plot(Vx/1e4, Dens)
+    Integral[i] = dx*(1/2)*(Densx[i]+Densx[i+1])
+print(sum(Integral)*N)
+print(integrate*N)
+"""plt.plot(Vx/1e4, Densx)
 plt.xlabel("10^4 m/s")
-plt.show()
+plt.show()"""
+Vx, Densx = Pvx(5e3, 30e3, N, 3000)
+dx = Vx[1]-Vx[0]
+integrate = np.trapz(Densx, Vx, dx)
+print(integrate)#0.08224984975151238 sannsynligheten for at vi finner partikler med gitt hastighet [5,30]*10**3
+print(integrate*N) #8224.984975151237 antall partikler mellom hastighetene [5,30]*10**3
+def Pv(a, b, N, T):
+    V = np.linspace(a, b, N)  # Hastighet for N partikler i x retning
+    m = 2*1.6e-27
+    K = 1.38*1e-23
+    o = (K*T)
+    sigma = np.sqrt(o/m)
+    dv=V[1]-V[0]
+    Dens = np.zeros(len(V))
+    Integral = np.zeros(len(V))
+    for i in range(len(V)-1):
+        # sannsynlighets fordeling for alle abs(V) verdier av N partikler
+        Dens[i] = ((m/(2*np.pi*K*T))**3/2)*np.exp(-m*V[i]**2/(2*K*T))*(4*np.pi*V[i]**2)
+        Integral[i] = dv*(1/2)*(Dens[i]+Dens[i+1])
+       #feil i utregning altfor lav sannsynlighet
+    
+    return V, Integral
+V,Dens=Pv(0,3e4, N, 3000)
+print(Dens)
+plt.plot(V, Dens)
+#plt.xlabel("10^4 m/s")
+plt.show() 
+
+
+#middelverdi (ikke ferdig)
+
+def Pv(a, b, N, T):
+    V = np.linspace(a, b, N)  # Hastighet for N partikler i x retning
+    m = 2*1.6e-27
+    K = 1.38*1e-23
+    o = (K*T)
+    sigma = np.sqrt(o/m)
+    dv=V[1]-V[0]
+    Dens = np.zeros(len(V))
+    for i in range(len(V)-1):
+        # sannsynlighets fordeling for alle abs(V) verdier av N partikler
+        Dens[i] = ((m/(2*np.pi*K*T))**3/2)*np.exp(-m*V[i]**2/(2*K*T))*(4*np.pi*V[i]**2)*V[i]
+    Integral=np.trapz(Dens, V, dv)
+       
+    
+    return V, Integral
+v,u=Pv(0,10*20,N,3000)
+print(u)
