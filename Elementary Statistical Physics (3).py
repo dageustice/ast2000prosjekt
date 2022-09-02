4a5cbdf6fce63049eeea683457d5df897af999f9
dageustice <73906850+dageustice@users.noreply.github.com>
2022-09-02 19:55:49 +0200

    Update Elementary Statistical Physics (3).py

--- Elementary Statistical Physics (3).py
+++ Elementary Statistical Physics (3).py
@@ -2,14 +2,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 import random
 N = 10**5
-K = 1.38*1e-23
-m = 2*1.6e-27
-T=3000
+K = 1.38*1e-23  # Boltzmannkonstanten
+m = 2*1.6e-27   # Massen til H_2, kilogram
+T=3000          # Temperaturen, Kelvin
 #middelverdi av abs(V)
 def Pvu(a, b, N, T):
     V = np.linspace(a, b, N)
-    m = 2*1.6e-27
-    K = 1.38*1e-23
     Dens = np.zeros(len(V))
     for i in range(len(V)):
         Dens[i] = (((m/(2*np.pi*K*T))**(3/2))*4*np.pi*(V[i]**2)*np.exp(-0.5*(m*V[i]**2/(K*T))))*V[i]
@@ -22,9 +20,6 @@ print(integral) #5739.779892994196 <v> middelverdi
 #Trykk fordeling?
 def Pp(a, b, N, T):
     V = np.linspace(a, b, N)  # Hastighet for N partikler i x retning
-    m = 2*1.6e-27
-    
-    K = 1.38*1e-23
     P = np.zeros(len(V))
     p = np.zeros(len(V))
     n= np.zeros(len(V))
@@ -38,8 +33,8 @@ dp=p[1]-p[0]
 integral=np.trapz(n,p,dp)
 K = 1.38*1e-23
 trykk=integral*N/3
-print(N*K*T)#4.1399999999999994e-15 (fasit)
-print(trykk)#4.139999999999476e-15 utregning for trykk
+print(N*K*T)    #4.1399999999999994e-15 (fasit)
+print(trykk)    #4.139999999999476e-15 utregning for trykk
 E=trykk/N*3/2
-print(E)#utregning fra trykk og bevegelses mengde
-print(3/2*K*T) #fasit
+print(E)        #utregning fra trykk og bevegelses mengde
+print(3/2*K*T)  #fasit
