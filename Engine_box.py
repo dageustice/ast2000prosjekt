14fe64083d0fc92744aa10b5a751e7baa89f441d
dageustice <73906850+dageustice@users.noreply.github.com>
2022-09-22 12:48:05 +0200

    Update Engine_box.py

--- Engine_box.py
+++ Engine_box.py
@@ -1,4 +1,5 @@
 # IKKE KODEMAL
+import matplotlib.pyplot as plt
 import numpy as np
 import random as rng
 from numba import jit
@@ -6,24 +7,24 @@ from ast2000tools import constants as cons
 
 @jit(nopython=True)
 def simulate(N, T, box, NozzleToBoxRatio, Time, dt):
-    sigma = np.sqrt(cons.k_B * T / cons.m_H2)  # StanDev for
-    # StanDev for maxwell-boltzmann distribusjonen
+    sigma = np.sqrt(cons.k_B * T / cons.m_H2)
+    # stan.avv. for maxwell-boltzmann distribusjonen
     nozzle_radius = box * NozzleToBoxRatio / 2
     # størrelse på radius i forhold til 1/2 bokslengde
-    momentum = 0
-    particles = 0
+    momentum = 0    #initialverdi for bevegelsesmengde
+    particles = 0   #initialverdi antall unnslippede partikler
     N_pos = np.zeros((Time, N, 3), dtype=np.float64)
     N_pos0 = np.zeros((N, 3), dtype=np.float64)
     # da har vi alle konstanter, nå kan vi loope:
     for i in range(N):  # lager tilfeldig fordelt partikler i boksen
         # når N er stor, vil partiklene ha en uniform fordeling.
-        N_pos0[i][0] = rng.random() * box
-        N_pos0[i][1] = rng.random() * box
-        N_pos0[i][2] = rng.random() * box
+        N_pos0[i][0] = rng.random() * box   # tilfeldig x pos.
+        N_pos0[i][1] = rng.random() * box   # tilfeldig y pos.
+        N_pos0[i][2] = rng.random() * box   # tilfeldig z pos.
     N_pos[0] = N_pos0  # initialposisjon til partiklene
     v0 = np.random.normal(0, sigma, (N, 3))  # Maxwell boltzmann fordeling av farten
-    for i in range(1, Time):
-        for j in range(N):
+    for i in range(1, Time):    #løper gjennom tidssteg
+        for j in range(N):      #løper gjennom partiklene
             N_pos[i][j] = N_pos[i - 1][j] + v0[j] * dt
             pos = N_pos[i][j]   # forkorter variabelen
             if pos[1] < 0:
@@ -39,24 +40,26 @@ def simulate(N, T, box, NozzleToBoxRatio, Time, dt):
             particles*cons.m_H2/(Time*dt))
 
 
-def fuel_burnt(ccArea, box, rocketthrustforce, fuelmass, fuel_consumption,
-               rocketmass, speedboost):
+def fuel_burned(ccArea, box, rocketthrustforce, fuelmass, fuel_consumption,
+                rocketmass, speedboost):
     B = ccArea/(box**2)     # tversnittarealet / boksareal = antall bokser
-    v = 0   # startfart til raketten
-    M = rocketmass + fuelmass
-    fuelburned = 0
-    i = 1
-    dt = 1e-3
-    while v <= speedboost:
-        a = B*rocketthrustforce/M
-        v += a*dt
+    a = np.zeros(int(1e8)); v = 0   # startfart til raketten
+    M = rocketmass + fuelmass   # totale massen
+    fuelburned = 0              # 0 drivstoff brent på starten
+    i = 0                       # indeksering i while-loopen
+    dt = 1e-3                   # tidsinterval sekunder
+    while v <= speedboost:      # stopper når vi har nådd ønsket hastighet
+        a[i] = B*rocketthrustforce/M
+        v += a[i]*dt
         fuelburned += B*fuel_consumption*dt  # fuel brukt øker for hver dt
         M -= B*fuel_consumption*dt  # massen minker for hver dt
         if fuelburned > fuelmass:
-
             print("Burned up all the fuel!")
             print(f"Final velocity: {v:.2f}")
             print(f"{i*dt:.0f} seconds after boosting")
+            a = a[:i]
             break
         i += 1
-    return fuelburned, fuelmass-fuelburned, (dt*i), v
+        fuelleft = fuelmass - fuelburned
+    a = a[:i]
+    return fuelburned, fuelleft, (dt*i), v, a
